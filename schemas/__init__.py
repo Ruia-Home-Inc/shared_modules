5df8eeffabf65d630e3315a84d5306ef55c7d81f
tom-george-tm <tom.george@thoughtminds.io>
2025-08-19 10:43:33 +0530

    refactor: updated check_ot to check_otp

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,5 +1,5 @@
 # Schemas Package 
 from .common import  UserData, UserResponse, UserTenantStatusResponse
-from .input_validate import check_comment, check_date_range, check_str, check_password, check_name, check_ot
+from .input_validate import check_comment, check_date_range, check_str, check_password, check_name, check_otp
 
-__all__ = ["UserData","UserResponse","UserTenantStatusResponse","check_comment", "check_date_range", "check_str", "check_password", "check_name", "check_ot"]
+__all__ = ["UserData","UserResponse","UserTenantStatusResponse","check_comment", "check_date_range", "check_str", "check_password", "check_name", "check_otp"]
